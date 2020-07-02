cec4a611bc1a03b0859d7ab72c912ededc6fd158
kdgyekye <kingsleygyeky@gmail.com>
2020-07-02 03:17:11 +0100

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,87 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import Button from './Components/button';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component{
+
+  constructor(props){
+
+    super(props);
+
+    this.state = {
+        current: '0',
+        previous: [],
+        afterOperation: false
+
+    };
+  }
+
+    reset = () => {
+        this.setState({current: '0', previous: [], afterOperation: false});
+    };
+
+    addToCurrent = (symbol) => {
+        if (["/", "*", "-", "+"].indexOf(symbol) > -1) {
+            let {previous} = this.state;
+            previous.push(this.state.current + symbol);
+            this.setState({previous, afterOperation: true});
+            console.log(this.state.previous);
+        }
+        else {
+            if ((this.state.current === "0" && symbol !== ".") || this.state.afterOperation  ) {
+                this.setState({current: symbol, afterOperation: false});
+            }
+            else {
+                this.setState({current: this.state.current + symbol});
+            }
+
+        }
+    };
+    calculate = () =>{
+    let {current, previous, afterOperation} = this.state;
+    if(previous.length > 0){
+      current = eval(String((previous[previous.length-1])+current));
+      afterOperation= true;
+      this.setState({current, previous: [], afterOperation});
+    }
+    };
+  render(){
+    const buttons = [
+        {symbol: 'C', cols: 3, action: this.reset},
+        {symbol: '/', cols: 1, action: this.addToCurrent},
+        {symbol: '7', cols: 1, action: this.addToCurrent},
+        {symbol: '8', cols: 1, action: this.addToCurrent},
+        {symbol: '9', cols: 1, action: this.addToCurrent},
+        {symbol: '*', cols: 1, action: this.addToCurrent},
+        {symbol: '4', cols: 1, action: this.addToCurrent},
+        {symbol: '5', cols: 1, action: this.addToCurrent},
+        {symbol: '6', cols: 1, action: this.addToCurrent},
+        {symbol: '-', cols: 1, action: this.addToCurrent},
+        {symbol: '3', cols: 1, action: this.addToCurrent},
+        {symbol: '2', cols: 1, action: this.addToCurrent},
+        {symbol: '1', cols: 1, action: this.addToCurrent},
+        {symbol: '+', cols: 1, action: this.addToCurrent},
+        {symbol: '0', cols: 2, action: this.addToCurrent},
+        {symbol: '.', cols: 1, action: this.addToCurrent},
+        {symbol: '=', cols: 1, action: this.calculate},
+
+    ];
+    return (
+      <div className="App">
+          {this.state.previous.length > 0?
+              <div className='float-previous'>{this.state.previous[(this.state.previous.length)-1]}</div>
+          :null}
+        <input type='text' className='results' value={this.state.current} /> <br/>
+
+          {buttons.map((btn, i) => {
+            return(
+                <Button symbol = {btn.symbol} columns={btn.cols} action={(symbol) => btn.action(symbol)} key={i}/>
+            )
+          })}
+      </div>
+    )
+  }
 }
 
 export default App;
